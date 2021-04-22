1f8ca1b33d28011210c3785a80e501a34d92984b
NelsonSantos07 <nelsonsantostj@hotmail.com>
2021-04-22 02:27:30 -0300

    repositories implementados

---
+++ src/main/java/com/nelson/bookstore/repositories/LivroRepository.java
@@ -0,0 +1,11 @@
+package com.nelson.bookstore.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.nelson.bookstore.domain.Livro;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Integer> {
+
+}
